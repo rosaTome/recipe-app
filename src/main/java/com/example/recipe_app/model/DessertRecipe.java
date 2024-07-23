7ec4f210a71de791f0ac529a091619931a6d148b
rosaTome <tomerosalinda@gmail.com>
2024-07-23 15:01:48 +0100

    dessert recipe model added

--- src/main/java/com/example/recipe_app/model/DessertRecipe.java
+++ src/main/java/com/example/recipe_app/model/DessertRecipe.java
@@ -1,4 +1,63 @@
 package com.example.recipe_app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class DessertRecipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String ingredients;
+    private String preparation;
+    private String cooking;
+
+//    Default constructor
+    public DessertRecipe() {}
+
+//    Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getPreparation() {
+        return preparation;
+    }
+
+    public void setPreparation(String preparation) {
+        this.preparation = preparation;
+    }
+
+    public String getCooking() {
+        return cooking;
+    }
+
+    public void setCooking(String cooking) {
+        this.cooking = cooking;
+    }
 }
